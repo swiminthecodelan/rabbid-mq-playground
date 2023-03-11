5cab01dcfdeaf6b8d6ce24b05dc029be4efc6ac4
tyler <1125931078@qq.com>
2023-03-11 16:11:52 +0800

    add:

--- src/main/java/consumer/CodeStyle.java
+++ src/main/java/consumer/CodeStyle.java
@@ -2,11 +2,13 @@ package consumer;
 
 /**
  * @author tyler
- * @date 2023/3/11 14:44 
+ * @date 2023/3/11 14:44
  */
 public class CodeStyle {
 
   private String a;
 
-  private Integer b;
+
+
+  private Integer c;
 }
